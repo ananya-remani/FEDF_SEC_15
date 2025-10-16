aff743faf4c2b6896ea828ab5d3489d5a14d2a1e
ananya-remani <2420030086@klh.edu.in>
2025-10-16 11:22:38 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+export const books = [
+    {title: "The Hobbit, The Lord of the Rings, and The Chronicles of Narnia", author: "Jane Austen", year: 1813, price: 900, availability: "in stock"},
+    {title: "1984", author: "George Orwell", year: 1949, price: 843, availability: "in stock"},
+    {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960, price: 670, availability: "out of stock"},
+    {title: "A Song of Ice and Fire", author: "J.K. Rowling", year: 1999, price: 690, availability: "in stock"}
+];
